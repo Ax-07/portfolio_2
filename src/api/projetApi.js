a6babfed8bb02d9b4727ae7b5b882aa03e52ef91
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-05-24 18:54:34 +0200

    mobif base url

--- src/api/projetApi.js
+++ src/api/projetApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const projetApi = createApi({
     reducerPath: "projetApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "src/assets/json" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "/src/assets/json" }),
     endpoints: (builder) => ({
         getProjet: builder.query({
             query: () => "/projets.json",
