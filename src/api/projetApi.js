43674454a046da4b4e627e70973db01ddba69ea8
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-05-25 11:41:07 +0200

    modif base URL

--- src/api/projetApi.js
+++ src/api/projetApi.js
@@ -1,14 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// export const projetApi = createApi({
+//     reducerPath: "projetApiJson",
+//     baseQuery: fetchBaseQuery({ baseUrl: "./src/assets/json" }),
+//     endpoints: (builder) => ({
+//         getProjetJson: builder.query({
+//             query: () => "/projets.json",
+//             providesTags: ["ProjetJson"],
+//         }),
+//     }),
+// });
 export const projetApi = createApi({
-    reducerPath: "projetApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "../assets/json" }),
+    reducerPath: 'projetApi',
+    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_REACT_APP_API_URL}/api/projet`}),
     endpoints: (builder) => ({
-        getProjet: builder.query({
-            query: () => "/projets.json",
-            providesTags: ["Projet"],
-        }),
+      getProjet: builder.query({ 
+        query: () => '/',
+        // Ajout d'un tag 'Projet' pour cette requête
+        providesTags: ['Projet']
+      }),
     }),
 });
 
-export const { useGetProjetQuery } = projetApi;
+export const { useGetProjetJsonQuery } = projetApi;
