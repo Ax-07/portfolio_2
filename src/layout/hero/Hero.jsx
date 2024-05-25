9d1291b9c8db39e780cab3e7c3791a53afe61a5d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-05-25 22:09:10 +0200

    ajout composant tag et delete separateline

--- src/layout/hero/Hero.jsx
+++ src/layout/hero/Hero.jsx
@@ -2,23 +2,19 @@ import faEnvelope from "/src/assets/icons/faEnvelope.png";
 import { Link } from "react-router-dom";
 import { TextAnimation } from "../../components/TextAnimation/TextAnimation";
 import { SlideInTextAnimation } from "../../components/TextAnimation/SlideInTextAnimation";
-import { SeparateLine } from "../../components/separateLines/SeparateLine";
+import { Tag } from "../../components/tag/Tag";
 
 export const Hero = () => {
   return (
     <section id="hero" className="hero">
       <div className="hero__wrapper">
-      {/* <SeparateLine /> */}
         <TextAnimation text="Affringue Xavier" />
         <SlideInTextAnimation text="Développeur web" />
-        {/* <h2 className="hero__sub-title">Développeur web</h2> */}
-        <span className="hero__tag tag">Disponible</span>
+        <Tag text={"Disponible"} />
       </div>
       <Link to="/contact" className={`hero__btn btn`}>
         Contactez moi <img src={faEnvelope} alt="" className="hero__btn-icon" />
       </Link>
-      {/* <SeparateLine /> */}
-
     </section>
   );
 };
