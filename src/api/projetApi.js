effad378bc9fc6cb814af779086e266b05da968c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-05-25 15:58:54 +0200

    chore: Update base URL and add json folder to public

--- src/api/projetApi.js
+++ src/api/projetApi.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const projetApi = createApi({
     reducerPath: "projetApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "src/assets/json" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "/" }),
     endpoints: (builder) => ({
         getProjet: builder.query({
-            query: () => "/projets.json",
+            query: () => "/json/projets.json",
             providesTags: ["Projet"],
         }),
     }),
